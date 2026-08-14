1f6fdd5dbaa8c1d76664e87fc0b47b6e1f18430b
Xegram <78540186+xegram-0@users.noreply.github.com>
2026-08-14 18:09:38 +0700

    Create 11.py

---
+++ leetcode/leetcode75/11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        result = 0
+        while left < right:
+            width = right - left
+            water = min(height[left], height[right]) * width
+            result = max(result, water)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return result
+
+  
