14b3c3ad28f7d7ac066dd759594acd840a6565ad
Xegram <78540186+xegram-0@users.noreply.github.com>
2026-08-22 20:18:25 +0700

    Update 3622.py

--- leetcode/3622.py
+++ leetcode/3622.py
@@ -6,3 +6,18 @@ class Solution:
         total = sumDigits + proDigits 
         return True if n % total == 0 else False
         # return if n % total == 0
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        x = n
+        digit_sum = 0
+        digit_product = 1
+
+        while x > 0:
+            digit = x % 10
+            digit_sum += digit
+            digit_product *= digit
+            x //= 10
+
+        total = digit_sum + digit_product
+
+        return n % total == 0
