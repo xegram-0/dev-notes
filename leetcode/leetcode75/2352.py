11a2cf1f0308156a66ecf723b0e6ec770d033e61
Xegram <78540186+xegram-0@users.noreply.github.com>
2026-08-24 22:37:46 +0700

    2352. Equal Row and Column Pairs
    Implement equalPairs method to count matching row-column pairs.

--- leetcode/leetcode75/2352.py
+++ leetcode/leetcode75/2352.py
@@ -7,3 +7,21 @@ class Solution:
         return answer
 
 # Reference
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        map = {}
+
+        for row in grid:
+            row_tuple = tuple(row)
+            if row_tuple in map:
+                map[row_tuple] += 1
+            else:
+                map[row_tuple] = 1
+        
+        pairs = 0
+        for col in zip(*grid):
+            if col in map:
+                pairs += map[col]
+
+
+        return pairs
