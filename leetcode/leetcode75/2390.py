97737a49eef6baa56251bd4c58c7fb86c8fa7c59
Xegram <78540186+xegram-0@users.noreply.github.com>
2026-08-25 23:46:46 +0700

    Create 2390.py

---
+++ leetcode/leetcode75/2390.py
@@ -0,0 +1,36 @@
+# First thought
+class Solution:
+    def removeStars(self, s: str) -> str:
+        newS = ""
+        for i in range(len(s)):
+            if s[i] != '*':
+                newS += s[i]
+            else:
+                newS = newS.replace(s[i - 1], '')
+        return newS
+
+
+# Solution
+class Solution:
+    def removeStars(self, s: str) -> str:
+        temp = []
+        for c in s:
+            if c != '*':
+                temp.append(c)
+            else:
+                temp.pop()
+        return ''.join(temp)
+
+
+# Reference 
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+
+        for ch in s:
+            if ch == "*":
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        return "".join(stack)
